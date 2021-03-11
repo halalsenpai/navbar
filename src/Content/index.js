77c0747e5104cfd63c4c3e615805bf94ba660072
halalsenpai <abdchaghtai@gmail.com>
2021-03-11 21:45:58 +0500

    custom navbar responsive

---
+++ src/Content/index.js
@@ -0,0 +1,140 @@
+import React from "react";
+import "./style.scss";
+
+const index = () => {
+  return (
+    <div>
+      <div className="container content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt delectus
+          nihil numquam officiis aliquam officia debitis molestiae iusto,
+          excepturi libero sint optio obcaecati recusandae nisi laudantium
+          maxime voluptatibus. Libero, dolore. Ipsum ipsam unde porro iure
+          excepturi temporibus odit ab ullam autem laudantium eligendi veritatis
+          nihil atque deserunt, culpa provident soluta labore vero obcaecati?
+          Quaerat, officia. Vero rerum error cupiditate commodi. Perspiciatis
+          repellat porro, quaerat aperiam non laudantium omnis odio
+          exercitationem adipisci ipsa vel quod sint eum dicta eveniet dolores
+          similique assumenda aspernatur officia. Nam quis, voluptas excepturi
+          perferendis molestias minus. Harum odio totam, nobis doloribus ducimus
+          magni sunt, ad, provident nihil magnam qui minus nisi beatae. Sapiente
+          voluptate hic labore iste libero. Corporis, ullam? Id maxime earum
+          sint quasi! Ea. Sed, aliquam. Laudantium, vero. Sint minima placeat
+          quisquam eligendi natus eos illo voluptatum, facere quidem possimus
+          officia distinctio laborum error rerum dolore suscipit voluptatibus
+          iure maiores? Exercitationem, animi? Modi, culpa! Error sed est
+          pariatur dolore recusandae voluptate labore impedit vitae vel
+          reiciendis voluptates beatae id excepturi quo cupiditate deserunt,
+          illum tempora libero officiis modi. Perspiciatis quisquam tenetur amet
+          optio voluptas! Laudantium nihil excepturi adipisci inventore dolorum
+          repellendus reiciendis similique, repellat eos dignissimos suscipit
+          voluptatem voluptatum eligendi quos! At corrupti eius delectus cum
+          laborum unde, id amet facere, blanditiis nemo ad? Iste temporibus
+          officia earum similique deleniti asperiores velit atque ex dignissimos
+          fugit praesentium illo, debitis delectus vel aliquam exercitationem
+          qui officiis explicabo amet architecto iure aspernatur minima. Ex,
+          iste laborum? Et unde ipsum commodi recusandae, dignissimos fugit
+          laboriosam totam animi asperiores esse voluptatum optio sed. Incidunt
+          modi tenetur laboriosam quo enim voluptatibus eligendi, placeat nam
+          qui iure aperiam! Molestias, ipsum! Consequatur deleniti harum eum
+          aliquam excepturi et quod, in consequuntur voluptatum animi laboriosam
+          inventore suscipit quasi ratione, ipsa illo eius? Commodi facilis
+          possimus excepturi voluptates voluptatum autem eum ipsa asperiores.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ullam
+          officiis libero nulla culpa qui consectetur. Tempora deleniti dolor
+          quisquam perspiciatis, fuga natus blanditiis vero facilis quasi
+          dolores doloremque esse! Qui quos necessitatibus, rerum est
+          perferendis repudiandae nam dignissimos cumque numquam, odio
+          doloremque eum saepe voluptate minus molestiae sint, impedit dicta
+          magnam delectus quam dolores explicabo? Quaerat molestiae itaque odit.
+          Beatae ab numquam laboriosam id unde reiciendis, accusantium, iusto
+          est nobis eum nulla, nostrum quisquam rerum consectetur sunt
+          necessitatibus. Numquam qui magnam impedit dolorum quidem nulla quas
+          iste aspernatur quaerat! Iste aliquam dolores a ducimus, labore minus
+          ex aperiam magnam nemo, ea illum illo aut perferendis veniam culpa
+          eligendi facere explicabo molestias repellendus dolor dolorum vitae
+          fugit. Aut, eligendi quas? Eaque ad ratione alias magnam beatae totam
+          molestiae, rem deleniti harum illo dolorum non dicta quod iste ipsam
+          eveniet necessitatibus perferendis, in architecto quibusdam commodi
+          aliquam pariatur. Provident, est quos.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          officiis aliquid quaerat repellat corrupti nostrum molestiae libero
+          soluta, reprehenderit magnam! Aliquam vero asperiores voluptates,
+          dolorum pariatur ab odio culpa laudantium! Possimus cum nostrum quis
+          laborum doloribus, libero eaque quasi, magnam ea amet maiores hic
+          mollitia rerum non enim dolor error quidem voluptas architecto
+          obcaecati quam? Eius repudiandae ab facilis necessitatibus! Dicta,
+          laboriosam minima sequi libero, repellat aliquam tenetur ducimus
+          similique mollitia, eveniet officia hic eligendi. Quidem deleniti,
+          atque facere voluptatum iste ex maxime soluta nam, rerum, dicta odit
+          unde quas. Non voluptate animi, autem, sint officiis dicta porro
+          beatae provident perspiciatis laudantium dolorem tempore dolores
+          maxime quibusdam pariatur ipsam? Omnis incidunt repellat dolorum
+          delectus modi. Officiis a doloribus earum vel. Odit laborum voluptates
+          eius at libero et quia esse enim velit. Laborum et dolore aliquid
+          recusandae consectetur pariatur, mollitia exercitationem iusto aliquam
+          optio maxime, quasi aperiam ut repudiandae molestias eligendi.
+          Numquam, modi. At delectus debitis non quis sed, rem asperiores
+          repellendus voluptatibus aliquid aspernatur aut laudantium fugiat
+          facilis repellat nihil minima, officia labore inventore ex
+          dignissimos. Ea exercitationem suscipit tempore. Quia atque, molestias
+          corrupti fugit est placeat quas repudiandae delectus a ipsam
+          repellendus esse assumenda harum nulla voluptatem. Odit eius possimus
+          dicta ex earum delectus fugiat cumque nulla error porro. At maxime
+          voluptas possimus et, quis libero iste aut provident itaque quam
+          neque! Eligendi quam qui hic eum, voluptas ullam voluptatum minus
+          temporibus dolor! Nobis repellat veritatis animi sed doloribus?
+          Suscipit optio, mollitia, debitis provident soluta quae error voluptas
+          asperiores eaque numquam commodi rem explicabo quaerat eius fugiat
+          doloremque unde voluptatum. Adipisci magnam rem fugit cupiditate
+          quidem doloribus odio quas. Perspiciatis ex aspernatur nemo? Error
+          iste libero, earum non cumque, omnis voluptas in nostrum accusamus
+          quibusdam quia nesciunt at provident, odio nemo mollitia. Fugit
+          incidunt nemo sunt expedita minima laudantium? Velit, incidunt quam
+          sit eligendi natus dolorum voluptatem, facilis totam ipsa quaerat
+          officiis repudiandae aut quae ea ex dolorem vero? Deleniti corrupti
+          voluptas velit temporibus saepe veritatis quis accusamus natus! Quo,
+          nisi. Vero, tempore. Cupiditate debitis quisquam impedit dolorem
+          fugiat officiis molestiae suscipit voluptatum? Quibusdam sed commodi
+          voluptatem suscipit praesentium aperiam, fugiat ipsum reprehenderit
+          consequatur ipsa enim repellat, voluptatum fugit. Fuga nostrum debitis
+          alias quas itaque a cum ipsum ab veniam adipisci sunt repudiandae,
+          earum assumenda voluptas odio ipsam velit. Architecto atque enim, est
+          suscipit nihil quisquam laborum dolore iste? Aliquam ipsa expedita
+          voluptatum qui assumenda ab nemo facilis, delectus illum nulla
+          obcaecati. Ipsam animi harum aliquid maxime odit, ullam excepturi
+          minus nihil repellendus placeat laudantium sunt voluptatum magnam id?
+          Dolore eos dolorum enim a deserunt illo! Doloribus corrupti aliquam
+          similique recusandae facere cupiditate officia ratione, praesentium,
+          eum sapiente, accusamus nobis dolorem? Inventore libero accusantium
+          iste odio aperiam enim doloribus. Ipsam, excepturi quod labore iusto
+          numquam nostrum eius recusandae! Reiciendis aliquid eaque, omnis
+          dolores dicta dignissimos quae aut corporis adipisci dolor iste
+          obcaecati repellat mollitia voluptate esse natus odio architecto? Quo
+          vel, sed libero iusto tempore, inventore enim facere quibusdam qui et
+          veniam neque doloribus veritatis autem debitis aperiam, accusamus
+          repellat praesentium maxime voluptatum accusantium laboriosam commodi
+          earum. Eligendi, maxime. Impedit quo tempore neque esse dolore
+          asperiores quae recusandae quod in temporibus. Neque nobis nemo
+          numquam, aliquam aut dignissimos inventore iure molestiae libero
+          beatae delectus temporibus quibusdam quia! Sit, accusamus. Obcaecati
+          dolorum rerum quis sequi sit est earum? Amet repellat id voluptatem!
+          Facilis obcaecati eveniet quam est doloremque optio. Nostrum nisi
+          optio quo asperiores, eos officiis ipsum? Beatae, culpa? Corporis?
+          Quod inventore rerum perferendis sapiente consequuntur voluptatibus
+          similique, architecto incidunt voluptatum ullam quam doloremque,
+          mollitia pariatur ipsam eum numquam! Eaque aliquam impedit repudiandae
+          aut labore veritatis et eveniet, libero atque?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default index;
---
+++ src/Navbar/index.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import "./style.scss";
+
+const Navbar = () => {
+  const [auth, setAuth] = useState(true);
+  return (
+    <header className="sticky-top">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Custom Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <div className="d-flex flex-wrap">
+                <li className="nav-item col-lg col-md-12 ">
+                  <a
+                    className="nav-link active pb-0"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item col-lg col-md-12 ">
+                  <a
+                    className="nav-link active pb-0"
+                    aria-current="page"
+                    href="#"
+                  >
+                    About
+                  </a>
+                </li>
+                <li className="nav-item col-lg col-md-12 ">
+                  <a
+                    className="nav-link active pb-0"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </div>
+            </ul>
+            <div className="d-lg-flex">
+              {auth ? (
+                <div className="navbar-nav">
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle text-light"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Signed In as
+                    </a>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdown"
+                    >
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Profile
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Dashboard
+                        </a>
+                      </li>
+                      <li>
+                        <hr className="dropdown-divider" />
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Sign Out
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </div>
+              ) : (
+                <div className="btn btn-primary">Sign up</div>
+              )}
+
+              <div className="btn btn-success">List your place</div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
